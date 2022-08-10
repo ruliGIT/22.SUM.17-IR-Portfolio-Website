2e6ca82112ec20b98d412f4ca10e1f92190053dc
ruliGIT <91031188+ruliGIT@users.noreply.github.com>
2022-08-10 19:33:39 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd 22.SUM.17-IR-Portfolio-Website
+cd pe-portfolio
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
