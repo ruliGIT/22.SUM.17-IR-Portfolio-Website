fc3c22fb59ef8807d0178e7c10c3aecd8531d1f5
ruliGIT <91031188+ruliGIT@users.noreply.github.com>
2022-12-01 15:15:16 -0500

    update to redeploy.sh
    - stop + remove running containers before redeployment

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+docker stop $(docker ps -aq)
+docker container rm $(docker container ls -aq)
 cd pe-portfolio
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
